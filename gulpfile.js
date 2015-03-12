1c933976e75819d5abe96844c73cf0cd23e80ae9
Per Erik <pererikfinstad@gmail.com>
2015-03-12 17:16:13 +0100

    Added autoprefixer and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp      	 = require('gulp');
 var webserver 	 = require('gulp-webserver');
 var autoprefixer = require('gulp-autoprefixer');
-var sass		 		 = require('gulp-ruby-sass');
+var sass		 = require('gulp-ruby-sass');
+var notify		 = require('gulp-notify');
  
 
 
@@ -17,11 +18,21 @@ gulp.task('webserver', function() {
 
 gulp.task('styles', function() {
 	return sass('src/sass/main.scss', { style: 'expanded' })
-	//	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(gulp.dest('public/app/css/'));
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest('public/app/css/'))
+		.pipe(notify({ message: 'Styles task complete without errors'}));
 });
 
+gulp.task('watch', function() {
+  gulp.watch('src/sass/*.scss', ['styles']);
+});
 
+// Run $ gulp for development
 gulp.task('default', ['webserver'], function() {
+	gulp.start('watch');
+});
+
+// Run $ gulp deploy for production
+gulp.task('deploy', ['webserver'], function() {
 	gulp.start('styles');
 });
