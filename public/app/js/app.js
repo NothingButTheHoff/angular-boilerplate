d0bb86c479553894f00f3f9a1ad0dc090673e7b2
Per Erik <pererikfinstad@gmail.com>
2015-03-13 10:23:50 +0100

    Added route provider

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,9 +1,18 @@
 'use strict';
 
-angular.module('angular-boilerplate', 
-	[
+angular.module('angular-boilerplate', [
 		'boilerplateControllers',
 		'boilerplateDirectives',
 		'ngRoute'
-	]
-);
+])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/example', {
+			templateUrl: 'app/views/example.html',
+			controller: 'ExampleCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
