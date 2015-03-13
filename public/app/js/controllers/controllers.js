0390ea12bfa638154322528801c503b332ab7bbd
Per Erik <pererikfinstad@gmail.com>
2015-03-13 12:34:09 +0100

    Added an example controller

--- public/app/js/controllers/controllers.js
+++ public/app/js/controllers/controllers.js
@@ -5,5 +5,9 @@ angular.module('boilerplateControllers', [] )
 
 .controller('IndexController', ['$scope', function($scope) {
 	$scope.pageTitle = "Angular Boilerplate v0.0.1";
-}]);
+}])
+
+.controller('ExampleCtrl', ['$scope', function($scope) {
+	$scope.title = "Example";
+}])
 
